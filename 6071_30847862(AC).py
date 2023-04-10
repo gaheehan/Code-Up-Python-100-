3df90bd1f5dff87c56f50897a001b0e1821adfa1
Gahee <saico0@hanmail.net>
2023-04-10 20:06:35 +0900

    Code Up: 6056
    6056 : [기초-논리연산] 참/거짓이 서로 다를 때에만 참 출력하기(설명)(py)
    
    2개의 정수값이 입력될 때,
    그 불 값(True/False) 이 서로 다를 때에만 True 를 출력하는 프로그램을 작성해보자.

---
+++ 6071_30847862(AC).py
@@ -0,0 +1,8 @@
+﻿# 0이 아니면 입력된 정수를 출력하고, 0이 입력되면 출력을 중단해보자.
+
+while True:
+    a = int(input())
+    if a == 0:
+        break
+
+    print(a)
---
+++ 6075_30848108(AC).py
@@ -0,0 +1,7 @@
+﻿n=int(input())
+
+i=0
+while i<=n:
+    print(i)
+    i+=1
+
